2cdd06a63aac211087e684cdbb56fab6c408a7fa
MH-Masud <mh_hotmail.com>
2024-09-27 16:26:26 +0600

    Parse Url
    Get Year & Month

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var http = require('http');
+const { text } = require('stream/consumers');
+var url = require('url');
+
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.writeHead(200, {'Content':'text/html'});
+    var q = url.parse(req.url, true).query;
+    var txt = '';
+    txt += q.year + " " + q.month;
+    res.end(txt)
 }).listen(8080);
